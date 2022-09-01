b3a4d7cd9f934e3c721aeb0d77637b26c9f7fbd0
Raj Patel <rajpatel131298@gmail.com>
2022-09-01 00:38:40 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Fragment, useState } from 'react'
+import 'h8k-components'
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import {AiOutlineArrowRight} from "react-icons/ai";
+import { image1, image2, image3, image4 } from './assets/images'
+import { Thumbs, Viewer } from './components'
+
+const title = 'Catalog Viewer'
+
+function App() {
+  const catalogsList = [
+    {
+      thumb: image1,
+      image: image1
+    },
+    {
+      thumb: image2,
+      image: image2
+    },
+    {
+      thumb: image3,
+      image: image3
+    },
+    {
+      thumb: image4,
+      image: image4
+    }
+  ]
+
+  const [ catalogs ] = useState([...catalogsList])
+  const [ activeIndex, setActiveIndex ] = useState(0)
+  const [ slideTimer, setSlideTimer ] = useState(null)
+  const [ slideDuration ] = useState(3000)
+  
+  const startSlidShow = (event) => {
+    if (event.target.checked === true) {
+      onSlideChange();
+    }
+    else if (slideTimer !== null && event.target.checked === false){
+      clearInterval(slideTimer);
+      setSlideTimer(null);
+    }
+  }
+
+  const onSlideChange = () => {
+    let interval = setInterval ( () => {
+      document.getElementById("next_button").click();
+    }, slideDuration);
+    setSlideTimer(interval);
+  }
+
+
+  return (
+    <Fragment>
+      <h8k-navbar header={ title }></h8k-navbar>
+      <div className='layout-column justify-content-center mt-75'>
+        <div className='layout-row justify-content-center'>
+          <div className='card pt-25'>
+            <Viewer catalogImage={ catalogs[activeIndex].image } />
+            <div className='layout-row justify-content-center align-items-center mt-20'>
+            <button 
+              className="icon-only outlined"
+              data-testid="prev-slide-btn"
+              onClick={() => {
+                let prevIndex = activeIndex-1;
+                if(prevIndex < 0){
+                  prevIndex = catalogs.length - 1;
+                }
+                setActiveIndex(prevIndex);
+              }}
+            >
+              <AiOutlineArrowLeft />
+            </button>
+              <Thumbs 
+                items={ catalogs } 
+                currentIndex={ activeIndex } 
+                setActiveIndex={setActiveIndex}
+              />
+            <button 
+              className="icon-only outlined"
+              data-testid="next-slide-btn"
+              id="next_button"
+              onClick={() => {
+                let nextIndex = activeIndex+1;
+                if(nextIndex >= catalogs.length){
+                  nextIndex = 0;
+                }
+                setActiveIndex(nextIndex); 
+              }}
+            >
+              {/* <i className="material-icons">arrow_forward</i> */}
+              <AiOutlineArrowRight />
+            </button>
+            </div>
+          </div>
+        </div>
+        <div className='layout-row justify-content-center mt-25'>
+          <input 
+            type='checkbox'
+            data-testid='toggle-slide-show-button'
+            onChange={(event) => {startSlidShow(event)}}
+          /> 
+          <label className='ml-6'>Start Slide Show</label>
+        </div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
+
